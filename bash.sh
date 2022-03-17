5455a2fafcd17af680efe90cc87e135674cef235
offLaneq <79511161+offLaneqq@users.noreply.github.com>
2022-03-17 02:43:45 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 mkdir KlymchukDmytro
 cd KlymchukDmytro
-mkdir VARA
-mkdir VARB
-cd VARB
-touch Dmytro.txt
-echo "ki418" > Dmytro.txt
-echo  " srk90512@gmail.com" >> Dmytro.txt
-cd ..
-mkdir VARC
-cd VARC
-touch Klymchuk.txt
-ls -l Klymchuk.txt > Klymchuk.txt
-echo  " srk90512@gmail.com" >> Klymchuk.txt
+touch name.txt
+echo "Dmytr" > name.txt
